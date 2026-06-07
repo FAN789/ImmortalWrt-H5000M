96c2e6a4dba80b4a3915a31944516ee13f795089
FAN789 <FAN789@users.noreply.github.com>
2026-06-07 08:04:02 +0800

    Require LuCI bootstrap theme

--- scripts/apply-package-options.sh
+++ scripts/apply-package-options.sh
@@ -22,6 +22,7 @@ append_config() {
 
 append_config <<'EOF'
 CONFIG_LUCI_LANG_zh_Hans=y
+CONFIG_PACKAGE_luci-theme-bootstrap=y
 CONFIG_PACKAGE_luci-app-package-manager=y
 CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
 CONFIG_PACKAGE_luci-i18n-firewall-zh-cn=y
